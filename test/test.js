734b02135013849e55402c97bf98101aadf11549
Francesco <francescoantoniotonini@gmail.com>
2016-07-30 00:34:05 +0200

    Code improvements to follow lint rules, mocha tests

---
+++ test/test.js
@@ -0,0 +1,146 @@
+/* gnuplotter
+ * this project is a fork of Richard Meadows's 'node-plotter'
+ *
+ */
+
+var should = require('should');
+var plot = require('../index.js').plot;
+
+function handleResult(error, stdout, stderr, cb) {
+	should.not.exist(error);
+	stderr.should.be.empty();
+
+	cb();
+}
+
+describe('Plot tests', function() {
+	it('Output1', function(done) {
+		plot({
+			data:		[3, 1, 2, 3, 4],
+			filename:	'test/output1.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output2', function(done) {
+		plot({
+			data:		[ 3, 1, 2, 3, 4 ],
+			filename:	'test/output2.png',
+			style:		'linespoints',
+			title:		'Example \'Title\', \\n runs onto multiple lines',
+			logscale:	true,
+			xlabel:		'time',
+			ylabel:		'length of string',
+			format:		'png',
+			finish: 	function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output3', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4 ] },
+			style:		'lines',
+			filename:	'test/output3.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output4', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4 ], 'line' : { 1: 5, 5: 6 } },
+			style:		'lines',
+			filename:	'test/output4.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output5', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4 ], 'line' : { 1: 5, 5: 6 } },
+			style:		'lines',
+			filename:	'test/output5.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output6', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4, 15, 3, 2, 4, 11 ], 'tick2' : [ 3, 10, 2, 30, 4, 15, 3, 20, 4, 11 ], 'line' : { 1: 5, 5: 6 } },
+			moving_avg:	4,
+			style:		'lines',
+			filename:	'test/output6.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output7', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4, 15, 3, 2, 4, 11 ], 'tick2' : [ 3, 10, 2, 30, 4, 15, 3, 20, 4, 11 ], 'line' : { 1: 5, 5: 6 } },
+			moving_max:	2,
+			style:		'lines',
+			filename:	'test/output7.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output8', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4, 15, 3, 2, 4, 11 ], 'tick2' : [ 3, 10, 2, 30, 4, 15, 3, 20, 4, 11 ], 'line' : { 1: 5, 5: 6 } },
+			moving_max:	2,
+			style:		'lines',
+			filename:	'test/output8.png',
+			nokey:		true,
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output9', function(done) {
+		plot({
+			title:		'bad_example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4, 15, 3, 2, 4, 11 ], 'tick2' : function(i) { return i; }, 'line' : { 1: 5, 5: 6 } },
+			moving_max:	2,
+			style:		'lines',
+			filename:	'test/output9.png',
+			nokey:		true,
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output10', function(done) {
+		plot({
+			title:		'example',
+			data:		{ 'temperature' : { 1357162672: 22, 1357162782: 23, 1357162892: 24 } },
+			time:		'hours',
+			style:		'linespoints',
+			filename:	'test/output10.png',
+			format:		'png',
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+
+	it('Output11 (SVG)', function(done) {
+		plot({
+			title:		'svg example',
+			data:		{ 'tick' : [ 3, 1, 2, 3, 4, 15, 3, 2, 4, 11 ], 'line' : { 1: 5, 5: 6 } },
+			style:		'lines',
+			filename:	'test/output11.svg',
+			format:		'svg',
+			nokey:		true,
+			finish: function(error, stdout, stderr) { handleResult(error, stdout, stderr, done); }
+		});
+	});
+});
